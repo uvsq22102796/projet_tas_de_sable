af2fd7e47c838fff6aaf8046755ddec36913fce4
uvsq22102796 <91280537+uvsq22102796@users.noreply.github.com>
2022-02-11 16:01:11 +0100

    Update tas_de_sable.py

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -2,7 +2,7 @@
 # groupe MI 3
 # ABALIL YASMINE
 # MENNOUR THOMAS  
-# ROMAIN ELIES
+# GOD ELIES
 #      GAEL
 # https://github.com/uvsq22101081/projet_tas_de_sable.git
 #########################################
